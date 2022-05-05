7a29e6dbac07a348896a0b6aad3ac1d667db86d1
MattiDragon <matias.koivikko@gmail.com>
2022-05-05 11:59:14 +0300

    Comments for classes

--- src/main/java/io/github/mattidragon/mconfig/config/Comment.java
+++ src/main/java/io/github/mattidragon/mconfig/config/Comment.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.RECORD_COMPONENT})
+@Target({ElementType.RECORD_COMPONENT, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Comment {
     String value();
